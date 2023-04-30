a39912b917b0f8e20edbaa85da2cce64c2d8c6d9
MXB683 <119166936+MXB683@users.noreply.github.com>
2023-04-30 15:50:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// © Copyright MXB. Do not ditribute!
+// All subject images/links are Copyright Free
+
+console.log("© Copyright MXB. Do not ditribute!\nAll subject images/links are Copyright Free")
+
+document.getElementById('l-theme-btn').addEventListener(
+  'click', () => {
+    document.documentElement.style.setProperty
+    ('--bg-color', 'white')
+  }
+)
+
+document.getElementById('d-theme-btn').addEventListener(
+  'click', () => {
+    document.documentElement.style.setProperty
+    ('--bg-color', '#333')
+  }
+) 
